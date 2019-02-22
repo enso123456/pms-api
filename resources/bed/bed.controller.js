932a152912c1e5657c0e9bf61a0a610ebf0eeeaa
Romeo Enso <romeo.enso@viseo.com>
2019-02-22 14:38:32 +0800

    add crud bed

--- resources/bed/bed.controller.js
+++ resources/bed/bed.controller.js
@@ -12,3 +12,33 @@ exports.addBed = async (req, res) => {
     throw new Error(e);
   }
 };
+
+exports.updateBed = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const name = req.body.name;
+
+    const updateBed = await models.bed.update({
+      name
+    }, {
+        where: {
+          id
+        }
+      });
+
+    res.json({ status: 200 });
+
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
+exports.deleteBed = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await models.bed.destroy({ where: { id } });
+    res.json({ status: 200 });
+  } catch (e) {
+    throw new Error(e);
+  }
+}
--- resources/bed/bed.router.js
+++ resources/bed/bed.router.js
@@ -6,7 +6,8 @@ const controller = require('../../resources/bed/bed.controller');
 router.get('/', (req, res) => {
   res.json({ status: 200 });
 });
-
 router.post('/', controller.addBed);
+router.put('/:id', controller.updateBed);
+router.delete('/:id', controller.deleteBed);
 
 module.exports = router;
