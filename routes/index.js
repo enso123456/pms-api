7dfca73a609089d91c245a392f12ebab35cd3660
Romeo Enso <romeo.enso@viseo.com>
2019-02-26 15:24:00 +0800

    added feature api route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const restRouter = express.Router();
 
 const bedRoutes = require('../resources/bed');
 const amenitiesRoutes = require('../resources/amenities');
+const featureRoutes = require('../resources/features');
 
 restRouter.use('/bed', bedRoutes);
 restRouter.use('/amenities', amenitiesRoutes);
+restRouter.use('/features', featureRoutes);
 
 module.exports = restRouter;
